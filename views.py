51aea5074ead55da8133a15644fa8a19abadc0a3
rudhifnd <rudhifnd@gmail.com>
2023-06-16 14:43:30 +0800

    update
    changed text fields to show nearest hotels

--- views.py
+++ views.py
@@ -1,8 +1,38 @@
-from flask import Blueprint, render_template
+from flask import Blueprint, render_template, jsonify
+import requests
 
 views = Blueprint(__name__,"views")
 
+api_key = 'AIzaSyD9pDE3krtYwqJhuCK4sEEeYSJVm5Q7JZU'
+latitude = 1.3121936729272838
+longitude = 103.8974076153921
+
+def get_nearby_hotels(latitude, longitude):
+    url = 'https://maps.googleapis.com/maps/api/place/nearbysearch/json'
+    params = {
+        'location': f'{latitude},{longitude}',
+        'radius': 500,
+        'type': 'lodging',
+        'key': api_key
+    }
+
+    response = requests.get(url, params=params)
+    data = response.json()
+
+    if response.status_code == 200:
+        hotels = data.get('results', [])
+        if hotels:
+            # latitudes = [hotel.get('geometry', {}).get('location', {}).get('lat') for hotel in hotels]
+            names = [hotel.get('name') for hotel in hotels]
+            return names
+        else:
+            print('No nearby hotels found.')
+    else:
+        print('Error occurred while retrieving nearby hotels.')
+        print('Response:', response.text)
+
 @views.route("/")  #this is to go to different types of pages url
-def home():
-    return render_template("index.html")  #will render the html templete to the route
+def home():  
+    test = get_nearby_hotels(latitude, longitude)
+    return render_template("index.html", hotel_names=test) # will render the html templete to the route
 
