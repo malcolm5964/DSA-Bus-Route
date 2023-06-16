70cf131f9b6eda6cfc8389faeaee8a83d15b391a
afiqster12 <2200546@sit.singaporetech.edu.sg>
2023-06-16 22:39:46 +0800

    added some form logic to list multiple hotel dropdown lists
    to do:
    add pin/markers to map

--- app.py
+++ app.py
@@ -2,9 +2,9 @@ from flask import Flask
 from views import views
 
 app = Flask(__name__)
-app.register_blueprint(views, url_prefix="/index") #this will enable to go the the specific pages example /views/home to go to home page or in this case /index to go to index page
-
-
+app.register_blueprint(views,
+                       url_prefix="/index")  # this will enable to go the the specific pages example /views/home to go to home page or in this case /index to go to index page
 
 if __name__ == '__main__':
-    app.run(debug=True, port=8000)  #to run the website in the port, debug=true is if any changes will automatically refresh and no need to rerun the script
+    app.run(debug=True,
+            port=8000)  # to run the website in the port, debug=true is if any changes will automatically refresh and no need to rerun the script
--- views.py
+++ views.py
@@ -1,11 +1,12 @@
 from flask import Blueprint, render_template, jsonify
 import requests
 
-views = Blueprint(__name__,"views")
+views = Blueprint(__name__, "views")
 
 api_key = 'AIzaSyD9pDE3krtYwqJhuCK4sEEeYSJVm5Q7JZU'
 latitude = 1.3121936729272838
 longitude = 103.8974076153921
+
 
 def get_nearby_hotels(latitude, longitude):
     url = 'https://maps.googleapis.com/maps/api/place/nearbysearch/json'
@@ -23,16 +24,40 @@ def get_nearby_hotels(latitude, longitude):
         hotels = data.get('results', [])
         if hotels:
             # latitudes = [hotel.get('geometry', {}).get('location', {}).get('lat') for hotel in hotels]
+            # names = [hotel.get('name') for hotel in hotels]
+            # return names
+            place_ids = [hotel.get('place_id') for hotel in hotels]
             names = [hotel.get('name') for hotel in hotels]
-            return names
+            hotels_dict = []
+            for i in range(len(place_ids)):
+                hotels_dict.append({"place_id": place_ids[i], "name": names[i]})
+            return hotels_dict
         else:
             print('No nearby hotels found.')
     else:
         print('Error occurred while retrieving nearby hotels.')
         print('Response:', response.text)
 
-@views.route("/")  #this is to go to different types of pages url
-def home():  
-    test = get_nearby_hotels(latitude, longitude)
-    return render_template("index.html", hotel_names=test) # will render the html templete to the route
 
+@views.route("/")  # this is to go to different types of pages url
+def home():
+    test = get_nearby_hotels(latitude, longitude)
+    return render_template("index.html", hotel_names=test,
+                           number_of_hotels=len(test))  # will render the html templete to the route
+
+
+@views.route('/data')
+def get_data():
+    hotel_names = get_nearby_hotels(latitude, longitude)
+    return jsonify(hotel_names=hotel_names)
+
+
+@views.route('/process_form', methods=['POST'])
+def process_form():
+    place_ids = request.form.get('arr[]')
+
+    # Process the form data
+    # ...
+
+    # Return a response
+    return 'Form data received'
