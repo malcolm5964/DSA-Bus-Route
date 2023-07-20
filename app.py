72cc91d5317c5de2a4a49db92e741186c54f4cf8
DeNhAiKal <115686752+DeNhAiKal@users.noreply.github.com>
2023-07-20 14:26:22 +0800

    Update on the url

--- app.py
+++ app.py
@@ -3,7 +3,7 @@ from views import views
 
 app = Flask(__name__)
 app.register_blueprint(views,
-                       url_prefix="/index")  # this will enable to go the the specific pages example /views/home to go to home page or in this case /index to go to index page
+                       url_prefix="/")  # this will enable to go the the specific pages example /views/home to go to home page or in this case /index to go to index page
 
 if __name__ == '__main__':
     app.run(debug=True,
